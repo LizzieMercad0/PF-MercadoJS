b2582c8643d92d8eaeaab131d318fe29847c34b2
Lizzie Mercado <lizzie.valentina.mercado20@gmail.com>
2024-08-07 21:03:43 -0300

    fix: resulve errores de linkeado de imagenes, errores por consola

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ botonAñadir.forEach(boton =>{
     boton.addEventListener("click",(e) =>{
         // Verificar si el usuario está iniciado
 if (localStorage.getItem('sesionIniciada') === 'true') {
-    window.location = "../pages/product.html"
+    window.location = "./pages/product.html"
 } else {
     Swal.fire({     //llamo a SweetAlert2 desde el html
         title: '¿Desea iniciar sesión?',
@@ -15,7 +15,7 @@ if (localStorage.getItem('sesionIniciada') === 'true') {
         confirmButtonText: 'Continuar',
         confirmButtonColor: "#658354"
     }).then(() => { //dirijo el boton al login.html
-        window.location = "./login.html"
+        window.location = "./pages/login.html"
     });
 }
     })
